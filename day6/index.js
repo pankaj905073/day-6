34dadb32568303c654568d8f63733d392a251022
pankaj905073 <vsr814271@gmail.com>
2024-12-02 15:43:09 +0530

    day 6

---
+++ day6/index.js
@@ -0,0 +1,42 @@
+//console.log("----hay!----JS----");
+
+//variable: var, let,const:
+//document.write("hallo pankaj")
+
+//let a = 5;
+//console.log(a)
+//var b = 5;
+//console.log(b)
+//const c = 10;
+//console.log(c)
+//a = 9;
+//b = 5;
+//c = 9;
+//if (a === 5) {
+  //  console.log("hello world im if condition");
+//}
+// loops: for and while:
+//print table of N:
+//print all Number : divisible by 3
+//function divisibleBy3and5(100) {
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0 ) {
+//         console.log("----- even -----", i)
+//     }
+// }
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 !== 0 ) {
+//         console.log("-----odd------", i)
+// }
+// }
+
+// let i=1 
+// while (i<=100){
+//   if(i%2==0){
+//   console.log("----even number----",i)
+//   }
+//   i++
+// }
+
+
+
